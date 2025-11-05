021ffe4f70d69cba02374f93199098f6e50fde75
Adrian-Vega-Espinoza <vegaadrian013@gmail.com>
2025-11-05 17:38:27 -0300

    Se avanzó con el diseño y estructura de register

---
+++ views/register.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro - Rose Notes.</title>
+    <link rel="stylesheet" href="../css/register.css">
+</head>
+<body>
+    <main>
+        <h1 class="tituloh1">REGISTRATE</h1>
+        <form class="formulario">
+            <label class="etiquetas-usuario">Nombre Completo</label>
+            <br>
+            <input type="text" class="campos-input">
+            <br>
+            <label class="etiquetas-usuario">Correo Electrónico</label>
+            <br>
+            <input type="email" class="campos-input" placeholder="example@gmail.com">
+            <br>
+            <label class="etiquetas-usuario">Contraseña</label>
+            <br>
+            <input type="password" class="campos-input">
+            <br>
+            <label class="etiquetas-usuario">Confirmar Contraseña</label>
+            <br>
+            <input type="password" class="campos-input">
+            <br>
+            <input type="button" class="boton-crear-cuenta" value="Crear Cuenta">
+            <br>
+            <p>¿Ya tenés cuenta?</p>
+            <br>
+            <input type="button" class="boton-inicio-sesion" value="Inicia Sesión">
+        </form>
+    </main>
+    <footer>
+      <div class="contenedorFooter">
+        <span>© 2025 RoseNote</span>
+      </div>
+    </footer>
+</body>
+</html>
