4dad16859e10ae23d5219ceec2b18f9781f39830
Caro-Brandon <1brandoncaro1@gmail.com>
2025-11-05 16:19:51 -0300

    subo x ahora (falta completar)

---
+++ views/home.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="css/home.css">
+<div class="contenedor ">
+    <h1 class="tituloh1">Tus ideas <br> florecen <br> acá</h1>
+
+    <span class="subtitulo">Ánota lo que pensas,Organiza tus dias <br>
+    y deja que florezca tus ideas </span>
+
+    <div class="contenedorBTN">
+       <button class="btn">Notas personales</button>
+       <button class="btn">Tareas</button>
+       <button class="btn">Inspiracion</button>
+    </div>
+ 
+</div>
--- views/navbar.php
+++ views/navbar.php
@@ -1,27 +1,37 @@
-<link rel="stylesheet" href="../css/navbar.css">
-
-<nav class="navbar navbar-expand-lg bg-transparent">
+ 
+<nav class="navbar navbar-expand-lg ">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a href="index.php" class="pe-13">
+    <img src="img/iconos/rosa.png" alt="Logo" width="50" height="50" class="logo">
+   </a>
+
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="#">RoseNote</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link active" aria-current="page" href="#">Sobre nosotros</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a class="nav-link active" aria-current="page" href="#">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Contacto</a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+           <!-- por el momento no lo vamos a usar en este<div class="botones">
+            <a href="soportePrincipal.php" class="btn">Iniciar Sesión</a>
+            <a href="logout.php" class="btn">Registrate</a>
+          </div> -->
     </div>
+ 
   </div>
 </nav>
