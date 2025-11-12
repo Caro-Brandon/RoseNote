a984b8e44210f41006d5d7913dcb2e9224f1dcbc
Caro-Brandon <1brandoncaro1@gmail.com>
2025-11-12 17:27:32 -0300

    suboa algo

---
+++ inspiracion.php
@@ -0,0 +1,5 @@
+<?php 
+// require_once "includes/config.php"; todavia no tenemos bd 
+$section = "views/inspiracion";
+require_once "views/layout.php";
+?>
--- js/list_to_do.js
+++ js/list_to_do.js
@@ -1,3 +1,20 @@
 /*
   hacer lista to do en js
  */
+
+
+const boton = document.getElementById("btn");
+const lista = document.getElementById("lista");
+
+function agregarElemento(boton,lista){  
+boton.addEventListener("click", function() {
+
+     const nuevoLi = document.createElement("li");
+    nuevoLi.innerHTML = "Nuevo Elemento";
+
+    lista.appendChild(nuevoLi);
+
+    alert("Se ha añadido un nuevo elemento");
+   });
+}
+agregarElemento(boton,lista);
